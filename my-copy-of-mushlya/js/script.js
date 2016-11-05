a1c1c29ef3aad5bf39367ab134c2d211e23c2209
Melnichenko Mykola <mykola.melnichenko@gmail.com>
2016-11-05 21:27:15 +0200

    first commit

---
+++ my-copy-of-mushlya/js/script.js
@@ -0,0 +1,16 @@
+// function initMap() {
+//     var location = {lat: 50.442823, lng: 30.521538};
+//     var mapDiv = document.getElementById('search_map');
+//     var map = new google.maps.Map(mapDiv, {
+//     center: location,
+//     zoom: 10,
+//     scrollwheel: false
+//     });
+
+//     new google.maps.Marker({
+//       position: location,
+//       map: map,
+//                   title: 'Fred&Fresh',
+//                   icon: 'img/icons_menu/hat.png'
+//     });
+// }
